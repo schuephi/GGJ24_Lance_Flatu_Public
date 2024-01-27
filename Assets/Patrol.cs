731333e7a1187e58a2cfdc1bf38f29bdb6d43514
Schüfi <philipp.schuerch@gmail.com>
2024-01-27 11:55:25 +0100

    basic waypoints

---
+++ Assets/Patrol.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GoonMovemenet Goon;
+    public List<Vector3> Waypoints= new List<Vector3>();
+    public int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        Waypoints = GetComponentsInChildren<Transform>().Select(x => x.position).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Goon.TargetPoint = Waypoints[currentWaypointIndex];
+        if((Goon.transform.position - Goon.TargetPoint).magnitude < 0.01)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= Waypoints.Count) { currentWaypointIndex = 0; }
+        }
+    }
+}
