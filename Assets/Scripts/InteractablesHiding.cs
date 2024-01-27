741e652f52c0e2826b363680f3281e4707f40633
Michel Knecht <michel.knecht@proton.me>
2024-01-27 14:35:40 +0100

    added testing scene for interactables

---
+++ Assets/Scripts/InteractablesHiding.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class InteractablesHiding : MonoBehaviour
+{
+    private bool isPlayerInside = false;
+    private bool isPlayerInReach = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerInside) // Leave hiding spot if already hidden
+        {
+            UnhidePlayer();
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !isPlayerInside && isPlayerInReach) // Enter Hiding spot if not already hidden and player is in reach
+        {
+            HidePlayer();
+        }
+    }
+
+    private void HidePlayer()
+    {
+        // Play hide animation
+        animator.SetBool("IsHidden", true);
+        isPlayerInside = true;
+    }
+
+    private void UnhidePlayer()
+    {
+        // Play unhide animation
+        animator.SetBool("IsHidden", false);
+        isPlayerInside = false;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInReach = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInReach = false;
+        }
+    }
+
+}
