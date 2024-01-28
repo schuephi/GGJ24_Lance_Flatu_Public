8d02bc6b815ff3ee74e3b6deab2f49cbb82dd844
Kreienbühl Tobias tkreienb <tkreienbuehl@quickline.ch>
2024-01-28 09:28:20 +0100

    Cars can drive now

---
+++ Assets/Scripts/Testing/TestLaunchCar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLaunchCar : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] positions;
+    [SerializeField]
+    private int[] directions;
+
+    private int currentIndex = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GameManager.Instance.LaunchCar(new Vector2(positions[currentIndex].position.x, positions[currentIndex].position.y), directions[currentIndex++ % positions.Length]);
+        }
+    }
+}
