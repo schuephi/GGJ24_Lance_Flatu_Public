776e325067e554454a022daca0ba7c284dc558f8
Kreienbühl Tobias tkreienb <tkreienbuehl@quickline.ch>
2024-01-28 12:50:55 +0100

    Immobility property to Lance

--- Assets/Scripts/CarManager.cs
+++ Assets/Scripts/CarManager.cs
@@ -11,22 +11,32 @@ public class CarManager : MonoBehaviour
     [SerializeField]
     private Vector2 carLaunchRange;
 
-    private bool isRunning;
-
     private void Start()
     {
-        isRunning = true;
+        StartCoroutine(DelayRegistration());
+    }
+
+    private IEnumerator DelayRegistration()
+    {
+        yield return new WaitForEndOfFrame();
+        GameManager.Instance.OnCarReachedGoal += Instance_OnCarReachedGoal;
+        StartCoroutine(LaunchCar());
+    }
+
+    private void Instance_OnCarReachedGoal()
+    {
         StartCoroutine(LaunchCar());
     }
 
     private IEnumerator LaunchCar()
     {
-        yield return new WaitForSeconds(1f);
-        while (isRunning)
-        {
-            var index = Random.Range(0, positions.Length);
-            GameManager.Instance.LaunchCar(new Vector2(positions[index].position.x, positions[index].position.y), directions[index]);
-            yield return new WaitForSeconds(Random.Range(carLaunchRange.x, carLaunchRange.y));
-        }
+        yield return new WaitForSeconds(Random.Range(carLaunchRange.x, carLaunchRange.y));
+        var index = Random.Range(0, positions.Length);
+        GameManager.Instance.LaunchCar(new Vector2(positions[index].position.x, positions[index].position.y), directions[index]);
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnCarReachedGoal -= Instance_OnCarReachedGoal;
     }
 }
--- Assets/Scripts/LanceScript.cs
+++ Assets/Scripts/LanceScript.cs
@@ -18,6 +18,7 @@ public class LanceScript : MonoBehaviour
     public bool isDead = false;
 
     public float Flatulence = 0;
+    public bool IsImmobile { get; private set; }
 
     // Start is called before the first frame update
     void Start()
